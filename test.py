a9a319fa6d30fd2692f9912f517d831b34a0687b
KTyanScavenger <kylabug@live.com>
2018-10-20 12:05:16 -0600

    Test.
    Use this in the game.

---
+++ test.py
@@ -0,0 +1,13 @@
+
+input1=input("Testing if this works")
+if input1=="yes":
+    input1=True 
+else:
+    input1=False
+
+
+newtest= input("True teset")
+if input1==True and newtest=="yes":
+    print("Hope this works")
+else:
+    print("Ha, loser.")
